79c490b5abf0599e9420a196073b6b31f257914f
Guy-Evens Gourdet <guygourdet7@gmail.com>
2022-10-31 14:42:11 -0400

    Created user entity

---
+++ src/main/java/com/cooksys/assessment/team2/api/SpringAssessmentSocialMediaSprint10Team2Application.java
@@ -0,0 +1,13 @@
+package com.cooksys.assessment.team2.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAssessmentSocialMediaSprint10Team2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAssessmentSocialMediaSprint10Team2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cooksys/assessment/team2/api/SpringAssessmentSocialMediaSprint10Team2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooksys.assessment.team2.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAssessmentSocialMediaSprint10Team2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
