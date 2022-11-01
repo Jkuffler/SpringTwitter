ff269d96a69b0681d864dd7ae6f7eb5a099ad237
Guy-Evens Gourdet <guygourdet7@gmail.com>
2022-11-01 16:47:58 -0400

    Updated POM file and configured UserMapper. Created first User and implemented first GET Endpoint.

--- src/main/java/com/cooksystems/assessment/team2/api/services/UserService.java
+++ src/main/java/com/cooksystems/assessment/team2/api/services/UserService.java
@@ -2,13 +2,12 @@ package com.cooksystems.assessment.team2.api.services;
 
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-
 import com.cooksystems.assessment.team2.api.dtos.UserResponseDto;
 
-@Service
 public interface UserService {
 
-	public List<UserResponseDto> getAllUsers();
+	List<UserResponseDto> getAllUsers();
+
+	
 
 }
