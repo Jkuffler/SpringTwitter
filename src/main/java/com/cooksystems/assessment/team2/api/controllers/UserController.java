9c670853e14d028e6ba61e02c5ac0d497a83dfae
Guy-Evens Gourdet <guygourdet7@gmail.com>
2022-11-03 00:07:03 -0400

    Implemented GET users (getAllUsers) endpoint.

--- src/main/java/com/cooksystems/assessment/team2/api/controllers/UserController.java
+++ src/main/java/com/cooksystems/assessment/team2/api/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.cooksystems.assessment.team2.api.controllers;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("users")
 public class UserController {
 	
+	@Autowired
 	private UserService userService;
 	
 	@GetMapping
