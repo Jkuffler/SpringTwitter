f867a396bd84ece380becdb11d48266c921270b6
Ryan Hatamosa <pinoydol.colada@gmail.com>
2022-11-04 17:34:46 -0400

    added getUserMentions and get tweet tags endpoints

--- src/main/java/com/cooksystems/assessment/team2/api/mappers/HashtagMapper.java
+++ src/main/java/com/cooksystems/assessment/team2/api/mappers/HashtagMapper.java
@@ -1,8 +1,15 @@
 package com.cooksystems.assessment.team2.api.mappers;
 
+import java.util.List;
+
 import org.mapstruct.Mapper;
+
+import com.cooksystems.assessment.team2.api.dtos.HashtagDto;
+import com.cooksystems.assessment.team2.api.entities.Hashtag;
 
 @Mapper(componentModel = "spring")
 public interface HashtagMapper {
+
+	List<HashtagDto> entitiesToResponseDtos(List<Hashtag> hashtagList);
 
 }
