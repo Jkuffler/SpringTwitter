0f44263e1a78055f201184a0052657545f9cee85
Guy-Evens Gourdet <guygourdet7@gmail.com>
2022-11-04 03:14:35 -0400

    Created validate skeleton

--- src/main/java/com/cooksystems/assessment/team2/api/services/impl/ValidateServiceImpl.java
+++ src/main/java/com/cooksystems/assessment/team2/api/services/impl/ValidateServiceImpl.java
@@ -2,10 +2,16 @@ package com.cooksystems.assessment.team2.api.services.impl;
 
 import org.springframework.stereotype.Service;
 
+import com.cooksystems.assessment.team2.api.repositories.UserRepository;
+
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class ValidateServiceImpl {
+	
+	private final UserRepository userRepository;
+	
+	
 
 }
